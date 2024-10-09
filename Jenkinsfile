dd4b41c90b16780984a5c109e5db5869fce3c183
byeongtaejeon <crossr95@gmail.com>
2024-10-10 06:25:55 +0900

    modify Acceptance test in Jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -41,8 +41,7 @@ pipeline {
         stage("Acceptance test") { 
             steps { 
                 sleep 60 
-                sh "./gradlew acceptanceTest -Dcalculator.url=http://localhost:8765"
-//                sh "chmod +x acceptance_test.sh && ./acceptance_test.sh"
+                sh "chmod +x acceptance_test.sh && ./acceptance_test.sh"
             } 
        }
     }
--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+#test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+
+#curl 결과를 result 변수에 저장
+result=$(docker exec calculator curl -s "http://localhost:8765/sum?a=1&b=2")
+
+# 기대값을 설정: 100+172=272
+expected=3
+
+# 결과값이 기대값과 동일한지 체크
+if [ "$result" -eq "$expected" ]; then
+    exit 0 #pass
+else
+    echo "Test failed. expected $expected, but result is $result"
+    exit 1
+fi
